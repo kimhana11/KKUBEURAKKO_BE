776e0e1cea8f9828637e1a921bb6fff8dc7c530c
김하나 <dd1246gksk@gmail.com>
2024-10-28 18:56:35 +0900

    setting: 프로젝트 초기설정

---
+++ src/main/java/com/example/kkubeurakko/KkubeurakkoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kkubeurakko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkubeurakkoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkubeurakkoApplication.class, args);
+    }
+
+}
