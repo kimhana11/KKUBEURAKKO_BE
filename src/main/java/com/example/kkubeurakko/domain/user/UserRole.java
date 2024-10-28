ec807b735b1fbd39a4d2681b418a0af3db4f8e95
김하나 <dd1246gksk@gmail.com>
2024-10-28 19:26:19 +0900

    Feat: user domain

---
+++ src/main/java/com/example/kkubeurakko/domain/user/UserRole.java
@@ -0,0 +1,21 @@
+package com.example.kkubeurakko.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserRole {
+
+    ADMIN("관리자",3),
+    DELIVERY("배달원",2),
+    CUSTOMER("일반회원",1),
+    GUEST("비회원",0);
+
+    private final String text;
+    private final int accessLevel;  // 권한 레벨
+
+    public boolean hasAccess(int requiredLevel) {
+        return this.accessLevel >= requiredLevel;
+    }
+}
