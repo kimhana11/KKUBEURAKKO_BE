bdfe09ee7e56827d773185051aa00502f53317a0
김하나 <dd1246gksk@gmail.com>
2024-11-03 19:18:40 +0900

    Feat: CouponType 추가

--- src/main/java/com/example/kkubeurakko/domain/coupon/Coupon.java
+++ src/main/java/com/example/kkubeurakko/domain/coupon/Coupon.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,9 +22,13 @@ public class Coupon extends BaseEntity {
 
     private String name; // 쿠폰 이름
     private BigDecimal discountAmount; // 할인 금액 (또는 비율)
-    private BigDecimal minOrderAmount; // 최소 주문 금액
     private LocalDateTime validFrom; // 쿠폰 유효 시작일
     private LocalDateTime validUntil; // 쿠폰 유효 종료일
+    private String code;
+    private String content; //쿠폰 세부 내용
+
+    @Enumerated(EnumType.STRING)
+    private CouponType couponType; // 쿠폰 종류
 
     @ManyToOne
     @JoinColumn(name = "store_id")
--- src/main/java/com/example/kkubeurakko/domain/store/Store.java
+++ src/main/java/com/example/kkubeurakko/domain/store/Store.java
@@ -27,11 +27,11 @@ public class Store extends BaseEntity {
     private String name;  //가게명
     private String address; //가게 위치
     private String phone; //가게 번호
-    private Time open_time;  //오픈 시간
-    private Time close_time; //마감시간
-    private Boolean is_open; //현재 영업 상태
-    private BigDecimal minimum_order_price; //최소주문금액
-    private Integer delivery_radius; //배달 가능 반경 km
+    private Time openTime;  //오픈 시간
+    private Time closeTime; //마감시간
+    private Boolean isOpen; //현재 영업 상태
+    private BigDecimal minOrderPrice; //최소주문금액
+    private Integer deliveryRadius; //배달 가능 반경 km
     private BigDecimal deliveryTip; //기본 배달비
     private double rating; //별점
 
