721ad4bc34cea1baec3b3105a102684fdb3df017
김하나 <dd1246gksk@gmail.com>
2024-11-05 21:39:54 +0900

    Feat: Address entity에 roadName(도로명) 추가

--- src/main/java/com/example/kkubeurakko/domain/coupon/CouponConditionType.java
+++ src/main/java/com/example/kkubeurakko/domain/coupon/CouponConditionType.java
@@ -1,15 +1,16 @@
 package com.example.kkubeurakko.domain.coupon;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum CouponConditionType {
     MIN_ORDER_AMOUNT("최소 주문 금액"),
-    SPECIFIC_MENU_ITEMS("특정 메뉴 필요");
+    SPECIFIC_MENU_ITEMS("특정 메뉴 주문시"),
+    FIRST_ORDER_DISCOUNT("첫 주문시"),
+    NO_CONDITIONS("조건없음");
 
     private final String description;
 
-    CouponConditionType(String description) {
-        this.description = description;
-    }
 }
