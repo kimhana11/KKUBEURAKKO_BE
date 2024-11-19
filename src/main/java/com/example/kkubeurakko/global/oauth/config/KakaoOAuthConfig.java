1340b95a3596af20e2cd1517bda156ec1384ed37
조아빈 <whdkqls122@naver.com>
2024-11-19 23:54:47 +0900

    feat : KakaoOAuthConfig 추가 KakaoProperties를 활용하여 OAuth2 클라이언트 설정 ClientRegistration을 설정하여 OAuth2 인증 정보 등 구성 InMemoryClientRegistrationRepostiory로 InMemory 방식으로 인증 하도록 구현

--- src/main/java/com/example/kkubeurakko/global/oauth/config/KakaoOAuthConfig.java
+++ src/main/java/com/example/kkubeurakko/global/oauth/config/KakaoOAuthConfig.java
@@ -1,2 +1,40 @@
-package com.example.kkubeurakko.global.oauth;public class KakaoOAuthConfig {
+package com.example.kkubeurakko.global.oauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+public class KakaoOAuthConfig {
+	private final KakaoProperties kakaoProperties;
+	@Autowired
+	public KakaoOAuthConfig(KakaoProperties kakaoProperties) {
+		this.kakaoProperties = kakaoProperties;
+	}
+	@Bean
+	public ClientRegistration kakaoClientRegistration() {
+		return ClientRegistrations
+			.fromIssuerLocation("https://kauth.kakao.com")
+			.registrationId("kakao")
+			.clientId(kakaoProperties.getClientId())
+			.clientSecret(kakaoProperties.getClientSecret())
+			.redirectUri(kakaoProperties.getRedirectUri())
+			.scope("account_email")
+			.authorizationUri("https://kauth.kakao.com/oauth/authorize")
+			.tokenUri("https://kauth.kakao.com/oauth/token")
+			.userInfoUri("https://kapi.kakao.com/v2/user/me")
+			.userNameAttributeName("id")
+			.clientName("Kakao")
+			.build();
+	}
+
+	@Bean
+	public InMemoryClientRegistrationRepository clientRegistrationRepository() {
+		return new InMemoryClientRegistrationRepository(kakaoClientRegistration());
+	}
 }
