eea5dae2ae1908866e58842b2c6ba6aeaa4132c4
조아빈 <whdkqls122@naver.com>
2024-11-28 19:52:00 +0900

    fix : role 변수타입 변경

--- src/main/java/com/example/kkubeurakko/global/oauth/dto/UserDto.java
+++ src/main/java/com/example/kkubeurakko/global/oauth/dto/UserDto.java
@@ -1,12 +1,14 @@
 package com.example.kkubeurakko.global.oauth.dto;
 
+import com.example.kkubeurakko.domain.user.UserRole;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class UserDto {
-	private String role;
+	private UserRole role;
 	private String nickname;
 	private String email;
 	private String userNumber;
