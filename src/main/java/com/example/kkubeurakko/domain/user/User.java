a53b26476e0f83666bf72fbbbac684e79f7d85a4
조아빈 <whdkqls122@naver.com>
2024-11-29 00:48:03 +0900

    feat : 빌더 추가 및 각 필드 column 어노테이션 추가하여 명시

--- src/main/java/com/example/kkubeurakko/domain/user/User.java
+++ src/main/java/com/example/kkubeurakko/domain/user/User.java
@@ -24,8 +24,16 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "userNumber", unique = true, nullable = false)
+    private String userNumber; //사용자 정보 제공자 + 제공 id 숫자
+
+    @Column(name = "email", nullable = false)
     private String email;
+
+    @Column(name = "phone", unique = true)
     private String phone;
+
+    @Column(name = "nickname", unique = true, nullable = false)
     private String nickname;
 
     @Enumerated(EnumType.STRING)
@@ -45,4 +53,15 @@ public class User extends BaseEntity {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<UserCoupon> userCoupons = new ArrayList<>();
+
+    @Builder
+    User(String userNumber, String email, String nickname, UserRole role){
+        this.userNumber = userNumber;
+        this.email = email;
+        this.nickname = nickname;
+        this.role = role;
+    }
+    User(String nickname){
+        this.nickname = nickname;
+    }
 }
