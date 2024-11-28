9b8909fd574c84313c7ccbc6b91bab124f442910
조아빈 <whdkqls122@naver.com>
2024-11-29 00:46:05 +0900

    feat : 요청한 유저 정보 저장 로직 추가 - 이미 등록된 사용자가 있는 경우 nickname업데이트 - 중복된 로직 수정

--- src/main/java/com/example/kkubeurakko/global/oauth/service/CustomOAuth2UserService.java
+++ src/main/java/com/example/kkubeurakko/global/oauth/service/CustomOAuth2UserService.java
@@ -8,17 +8,22 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import com.example.kkubeurakko.domain.user.User;
+import com.example.kkubeurakko.domain.user.UserRole;
+import com.example.kkubeurakko.domain.user.repository.UserRepository;
 import com.example.kkubeurakko.global.oauth.dto.CustomOAuth2User;
 import com.example.kkubeurakko.global.oauth.dto.KakaoResponse;
 import com.example.kkubeurakko.global.oauth.dto.OAuth2Response;
 import com.example.kkubeurakko.global.oauth.dto.UserDto;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
-
+	private final UserRepository userRepository;
 	@Override
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
@@ -43,10 +48,36 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 			return null;
 		}
 		String userNumber = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
-		String nickname = oAuth2Response.getNickname();
 		String email = oAuth2Response.getEmail();
-		UserDto userDto = new UserDto("ROLE_USER", nickname, email, userNumber);
+		String nickname = oAuth2Response.getNickname();
+		User existUser = userRepository.findByUserNumber(userNumber);
+		if(existUser == null){
+			User user = User.builder()
+				.userNumber(userNumber)
+				.email(email)
+				.nickname(nickname)
+				.role(UserRole.CUSTOMER)
+				.build();
 
-		return new CustomOAuth2User(userDto);
+			userRepository.save(user);
+			UserDto userDto = new UserDto(UserRole.CUSTOMER, nickname, email, userNumber);
+
+			return new CustomOAuth2User(userDto);
+		} else{
+			existUser = User.builder()
+				.nickname(nickname)			//닉네임 변경 가능성 염두
+				.build();
+			userRepository.save(existUser);
+
+			UserDto userDto = new UserDto(
+				existUser.getRole(),
+				nickname,
+				existUser.getEmail(),
+				existUser.getUserNumber()
+			);
+
+			return new CustomOAuth2User(userDto);
+		}
+
 	}
 }
