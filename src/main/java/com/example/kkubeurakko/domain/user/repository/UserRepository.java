37ce67cce9868fac48c63be5faece5915f9c79a7
조아빈 <whdkqls122@naver.com>
2024-11-29 00:48:35 +0900

    feat : 유저 찾는 메서드 추가

--- src/main/java/com/example/kkubeurakko/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/kkubeurakko/domain/user/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.kkubeurakko.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	User findByUserNumber(String userNumber);
 }
