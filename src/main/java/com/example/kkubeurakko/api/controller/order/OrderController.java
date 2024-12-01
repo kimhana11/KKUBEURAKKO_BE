498dfcf3f7a9654b8734e0901df9b80ead0a32d5
김하나 <dd1246gksk@gmail.com>
2024-12-01 22:14:08 +0900

    Feat: OrderController 생성

--- src/main/java/com/example/kkubeurakko/api/controller/order/OrderController.java
+++ src/main/java/com/example/kkubeurakko/api/controller/order/OrderController.java
@@ -1,4 +1,18 @@
 package com.example.kkubeurakko.api.controller.order;
 
+import com.example.kkubeurakko.api.controller.order.request.OrderStatusRequest;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderController {
+
+
+    @MessageMapping("/start")
+    @SendTo("/topic/orders")
+    public void startOrder(OrderStatusRequest orderStatus) {
+        // 주문 상태를 '접수'로 변경
+       // return null;
+    }
 }
