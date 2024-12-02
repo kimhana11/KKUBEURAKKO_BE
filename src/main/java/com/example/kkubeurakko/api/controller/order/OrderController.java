3974b7a679a6448901fde63966c1556add09cb71
김하나 <dd1246gksk@gmail.com>
2024-12-02 17:11:40 +0900

    Feat: OrderStatus 변경 컨트롤러 작성

--- src/main/java/com/example/kkubeurakko/api/controller/order/OrderController.java
+++ src/main/java/com/example/kkubeurakko/api/controller/order/OrderController.java
@@ -1,18 +1,23 @@
 package com.example.kkubeurakko.api.controller.order;
 
 import com.example.kkubeurakko.api.controller.order.request.OrderStatusRequest;
+import com.example.kkubeurakko.domain.order.Order;
+import com.example.kkubeurakko.domain.order.OrderStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class OrderController {
 
 
     @MessageMapping("/start")
-    @SendTo("/topic/orders")
-    public void startOrder(OrderStatusRequest orderStatus) {
-        // 주문 상태를 '접수'로 변경
-       // return null;
+    public void startOrder(OrderStatusRequest orderStatusRequest) {
+        // 주문 상태를 'RECEIVED'로 변경
+        Order updatedOrder = orderService.updateOrderStatus(orderStatusRequest.getOrderId(), OrderStatus.RECEIVED);
+
+        // 변경된 주문 정보를 소켓 구독자들에게 전달
+        messagingTemplate.convertAndSend("/topic/orders", updatedOrder);
     }
 }
