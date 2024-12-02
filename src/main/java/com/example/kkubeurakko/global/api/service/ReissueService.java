d0757e4d78bfd1d8b70c383f1d453eaaabae6846
조아빈 <whdkqls122@naver.com>
2024-12-02 19:21:25 +0900

    feat : Reissue 서비스 로직 추가 - 쿠키에서 refresh 토큰 추출 메서드 추가 - refresh토큰 검증 후 access토큰 재발급 메서드 추가 - 예외처리 로직 추가구현 예정

--- src/main/java/com/example/kkubeurakko/global/api/service/ReissueService.java
+++ src/main/java/com/example/kkubeurakko/global/api/service/ReissueService.java
@@ -1,2 +1,52 @@
-package com.example.kkubeurakko.global.api.service;public class ReissueService {
+package com.example.kkubeurakko.global.api.service;
+
+import com.example.kkubeurakko.global.jwt.JwtUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReissueService {
+
+	private final JwtUtil jwtUtil;
+
+	// 리프레시 토큰 추출
+	public String extractRefreshTokenFromCookies(HttpServletRequest request) {
+		Cookie[] cookies = request.getCookies();
+		if (cookies == null) {
+			return null;										//예외를 던지도록 업데이트 예정
+		}
+
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals("Authorization")) {
+				return cookie.getValue();
+			}
+		}
+		return null;											//예외를 던지도록 업데이트 예정
+	}
+
+	// 리프레시 토큰 검증 및 액세스 토큰 새로 발급
+	public String processTokenReissue(String refreshToken) {
+		// 토큰 검증
+		try {
+			jwtUtil.isExpired(refreshToken);
+		} catch (ExpiredJwtException e) {
+			throw new IllegalArgumentException("refresh token expired");
+		}
+
+		String category = jwtUtil.getCategory(refreshToken);
+		if (!"refresh".equals(category)) {
+			throw new IllegalArgumentException("invalid refresh token");
+		}
+
+		// 유저 정보 추출
+		String userNumber = jwtUtil.getUserNumber(refreshToken);
+		String role = jwtUtil.getRole(refreshToken);
+
+		// 새로운 액세스 토큰 생성
+		return jwtUtil.createJwt("access", userNumber, role, 60*60*60L);
+	}
 }
