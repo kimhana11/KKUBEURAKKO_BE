ba97ec03bef14853823b6973433eec6eb275e501
조아빈 <whdkqls122@naver.com>
2024-12-06 18:14:51 +0900

    refactor : CorsMvcConfig로 cors 설정 통합 하도록 수정

--- src/main/java/com/example/kkubeurakko/global/config/SecurityConfig.java
+++ src/main/java/com/example/kkubeurakko/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.kkubeurakko.global.config;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
@@ -34,26 +36,6 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 		http
-			.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-
-				@Override
-				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-
-					CorsConfiguration configuration = new CorsConfiguration();
-
-					configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-					configuration.setAllowedMethods(Collections.singletonList("*"));
-					configuration.setAllowCredentials(true);
-					configuration.setAllowedHeaders(Collections.singletonList("*"));
-					configuration.setMaxAge(3600L);
-
-					configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
-					configuration.setExposedHeaders(Collections.singletonList("Authorization"));
-
-					return configuration;
-				}
-			}));
-		http
 			.csrf((auth) -> auth.disable());
 		http
 			.formLogin((auth) -> auth.disable());
@@ -69,17 +51,20 @@ public class SecurityConfig {
 				new CustomLogoutFilter(jwtUtil, refreshTokenRepository),
 				LogoutFilter.class
 			);
+		//JWTFilter 추가
+		http
+			.addFilterAfter(new JwtFilter(jwtUtil), OAuth2LoginAuthenticationFilter.class);
 		http
 			.oauth2Login(
 				((oauth2) -> oauth2
-				.userInfoEndpoint((userInfoEndpointConfig) ->
-					userInfoEndpointConfig.userService(customOAuth2UserService))
+					.userInfoEndpoint((userInfoEndpointConfig) ->
+						userInfoEndpointConfig.userService(customOAuth2UserService))
 					.successHandler(customSuccessHandler)
 				)
 			);
 		http
 			.authorizeHttpRequests((auth) -> auth
-				.requestMatchers("/", "/reissue").permitAll()
+				.requestMatchers("/", "/reissue", "/access").permitAll()
 				.anyRequest().authenticated()
 			);
 		http
