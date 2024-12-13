fd747faedde49efe88c6be49fe99cfcccd6fb03f
조아빈 <whdkqls122@naver.com>
2024-12-13 15:39:46 +0900

    feat : 주소 entity <-> dto 변환 클래스 및 인터페이스 추가

--- src/main/java/com/example/kkubeurakko/api/address/mapper/AddressMapper.java
+++ src/main/java/com/example/kkubeurakko/api/address/mapper/AddressMapper.java
@@ -1,2 +1,17 @@
-package com.example.kkubeurakko.api.address.mapper;public interface AddressMapper {
+package com.example.kkubeurakko.api.address.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+
+import com.example.kkubeurakko.api.address.controller.request.AddressRequest;
+import com.example.kkubeurakko.api.address.controller.response.AddressResponse;
+import com.example.kkubeurakko.domain.address.Address;
+import com.example.kkubeurakko.domain.user.User;
+
+@Mapper
+public interface AddressMapper {
+	AddressResponse addressToAddressResponse(Address address);
+	Address addressRequestToAddress(AddressRequest addressRequest, User user);
+	List<AddressResponse> addressListToAddressResponseList(List<Address> addressList);
 }
