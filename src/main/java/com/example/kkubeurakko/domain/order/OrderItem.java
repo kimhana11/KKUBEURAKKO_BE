205a58ad9365f33ac27c6087179763e5f501c1fc
김하나 <dd1246gksk@gmail.com>
2024-12-13 22:51:35 +0900

    Feat: OrderItem 테이블에 quantity(수량) 컬럼 추가

--- src/main/java/com/example/kkubeurakko/domain/order/OrderItem.java
+++ src/main/java/com/example/kkubeurakko/domain/order/OrderItem.java
@@ -16,7 +16,7 @@ public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    private int quantity;
     private BigDecimal totalPrice;
 
     @ElementCollection
