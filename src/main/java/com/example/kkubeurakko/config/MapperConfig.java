639a52c5f055e1e10ea7c34e9d13724b11e27a03
조아빈 <whdkqls122@naver.com>
2024-12-13 15:40:25 +0900

    feat : map struct 활용을 위한 설정클래스 추가

--- src/main/java/com/example/kkubeurakko/config/MapperConfig.java
+++ src/main/java/com/example/kkubeurakko/config/MapperConfig.java
@@ -1,2 +1,15 @@
-package com.example.kkubeurakko.config;public class MapperConfig {
+package com.example.kkubeurakko.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.kkubeurakko.api.address.mapper.AddressMapper;
+import com.example.kkubeurakko.api.address.mapper.AddressMapperImpl;
+
+@Configuration
+public class MapperConfig {
+	@Bean
+	public AddressMapper addressMapper(){
+		return new AddressMapperImpl();
+	}
 }
