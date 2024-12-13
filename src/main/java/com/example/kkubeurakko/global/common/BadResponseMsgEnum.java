0c97ff79fcd34a313b3edd97a27da383d0b9f531
조아빈 <whdkqls122@naver.com>
2024-12-13 16:18:20 +0900

    feat : 전체 정상응답 메시지 및 에러 메시지 enum 클래스 추가

--- src/main/java/com/example/kkubeurakko/global/common/BadResponseMsgEnum.java
+++ src/main/java/com/example/kkubeurakko/global/common/BadResponseMsgEnum.java
@@ -5,8 +5,12 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum ResponseMsgEnum {
+public enum BadResponseMsgEnum {
+	//조회시 null 506
+	//주소 관련 응답
+	ADDRESS_NULL(506, "조회된 주소가 없습니다."),
 
+	//
 	JWT_ACCESS_EXPIRED(401, "access토큰이 만료되었습니다."),
 	JWT_ACCESS_NULL(401, "access토큰이 없습니다."),
 	JWT_REFRESH_EXPIRED(500, "refresh토큰이 만료되었습니다."),
--- src/main/java/com/example/kkubeurakko/global/common/ResponseMsgEnum.java
+++ src/main/java/com/example/kkubeurakko/global/common/ResponseMsgEnum.java
@@ -1,2 +1,15 @@
-package com.example.kkubeurakko.global.common;public enum SuccessResponseMsgEnum {
+package com.example.kkubeurakko.global.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ResponseMsgEnum {
+	SAVE_ADDRESS_SUCCESS(200, "추가되었습니다."),
+	UPDATE_ADDRESS_SUCCESS(200, "수정되었습니다."),
+	DELETE_ADDRESS_SUCCESS(200, "삭제되었습니다.");
+
+	private int code;
+	private String responseMsg;
 }
