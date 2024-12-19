12da8c49beb52df3e5fdad776005cede8c18d6e9
조아빈 <whdkqls122@naver.com>
2024-12-19 16:06:15 +0900

    refactor : cors 설정 추가 및 필터에서 발생하는 예외처리 추가

--- src/main/java/com/example/kkubeurakko/global/config/SecurityConfig.java
+++ src/main/java/com/example/kkubeurakko/global/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.kkubeurakko.global.config;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
@@ -13,7 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.example.kkubeurakko.global.jwt.CustomLogoutFilter;
 import com.example.kkubeurakko.global.jwt.JwtFilter;
@@ -21,8 +19,8 @@ import com.example.kkubeurakko.global.jwt.JwtUtil;
 import com.example.kkubeurakko.global.jwt.RefreshTokenRepository;
 import com.example.kkubeurakko.global.oauth.handler.CustomSuccessHandler;
 import com.example.kkubeurakko.global.oauth.service.CustomOAuth2UserService;
-
-import jakarta.servlet.http.HttpServletRequest;
+import com.example.kkubeurakko.global.security.GlobalAccessDeniedHandler;
+import com.example.kkubeurakko.global.security.GlobalAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -33,8 +31,26 @@ public class SecurityConfig {
 	private final CustomSuccessHandler customSuccessHandler;
 	private final JwtUtil jwtUtil;
 	private final RefreshTokenRepository refreshTokenRepository;
+	private final GlobalAuthenticationEntryPoint authenticationEntryPoint;
+	private final GlobalAccessDeniedHandler accessDeniedHandler;
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http
+			.cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
+
+				CorsConfiguration configuration = new CorsConfiguration();
+
+				configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+				configuration.setAllowedMethods(Collections.singletonList("*"));
+				configuration.setAllowCredentials(true);
+				configuration.setAllowedHeaders(Collections.singletonList("*"));
+				configuration.setMaxAge(3600L);
+
+				configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+				configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+				return configuration;
+			}));
 		http
 			.csrf((auth) -> auth.disable());
 		http
@@ -61,6 +77,11 @@ public class SecurityConfig {
 						userInfoEndpointConfig.userService(customOAuth2UserService))
 					.successHandler(customSuccessHandler)
 				)
+			);
+		http
+			.exceptionHandling(exceptionHandling -> exceptionHandling
+				.authenticationEntryPoint(authenticationEntryPoint)
+				.accessDeniedHandler(accessDeniedHandler)
 			);
 		http
 			.authorizeHttpRequests((auth) -> auth
