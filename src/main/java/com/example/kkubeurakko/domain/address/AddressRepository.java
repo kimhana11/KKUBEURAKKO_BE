a081dd784de3b220b40a13c8cc8a3e97afb17bb3
조아빈 <whdkqls122@naver.com>
2024-12-19 16:05:12 +0900

    feat: AddressRepository에 사용자별 주소 및 기본 주소 조회 기능 추가

--- src/main/java/com/example/kkubeurakko/domain/address/AddressRepository.java
+++ src/main/java/com/example/kkubeurakko/domain/address/AddressRepository.java
@@ -1,2 +1,20 @@
-package com.example.kkubeurakko.domain.address;public interface AddressRepository {
+package com.example.kkubeurakko.domain.address;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.kkubeurakko.domain.user.User;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+	Optional<List<Address>> findAllByUser(User user);
+	@Query("select a from Address a where a.user = :user and a.isPrimary = true")
+	Optional<Address> findPrimaryAddressByUser(@Param("user") User user);
+
+	boolean existsByUser(@Param("user") User user);
 }
