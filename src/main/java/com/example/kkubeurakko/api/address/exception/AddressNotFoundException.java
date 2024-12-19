88a9a137c38ed1946be546bb736ab3786325aa9b
조아빈 <whdkqls122@naver.com>
2024-12-19 15:57:34 +0900

    feat : 주소를 찾지 못했을때 발생시키는 customexception 추가 - 명확하게 어떠한 예외인지 알도록 하기 위해 구현

--- src/main/java/com/example/kkubeurakko/api/address/exception/AddressNotFoundException.java
+++ src/main/java/com/example/kkubeurakko/api/address/exception/AddressNotFoundException.java
@@ -1,2 +1,13 @@
-package com.example.kkubeurakko.api.address.exception;public class AddressNotFoundException {
+package com.example.kkubeurakko.api.address.exception;
+
+import com.example.kkubeurakko.global.common.BadResponseMsgEnum;
+import com.example.kkubeurakko.global.exception.GlobalException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AddressNotFoundException extends GlobalException {
+	public AddressNotFoundException(){
+		super(BadResponseMsgEnum.ADDRESS_NOT_FOUND);
+	}
 }
