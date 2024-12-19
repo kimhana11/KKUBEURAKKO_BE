3ab68869b8f70269da94202855db2b304ea603a4
조아빈 <whdkqls122@naver.com>
2024-12-19 16:03:24 +0900

    feat : 수정 메서드 및 빌더 생서자 추가 - 수정 시 매개변수로 dto를 받도록 구현

--- src/main/java/com/example/kkubeurakko/domain/address/Address.java
+++ src/main/java/com/example/kkubeurakko/domain/address/Address.java
@@ -1,21 +1,26 @@
 package com.example.kkubeurakko.domain.address;
 
+import com.example.kkubeurakko.api.address.controller.request.AddressRequest;
 import com.example.kkubeurakko.domain.BaseEntity;
 import com.example.kkubeurakko.domain.user.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Address extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String addressNickname;
     private String roadName;
     private String detailedAddress;
     private String postalCode;
@@ -28,5 +33,27 @@ public class Address extends BaseEntity {
     // 기본 주소 선택
     public void setSelected(Boolean selected) {
         this.isPrimary = selected;
+    }
+
+    // 기본 주소 업데이트
+    public void updatePrimary(boolean isPrimary){
+        this.isPrimary = isPrimary;
+    }
+    public void updateAddress(Long addressId, AddressRequest addressRequest){
+        this.id = addressId;
+        this.addressNickname = addressRequest.addressNickname();
+        this.roadName = addressRequest.roadName();
+        this.detailedAddress = addressRequest.detailedAddress();
+        this.postalCode = addressRequest.postalCode();
+        this.isPrimary = addressRequest.isPrimary();
+    }
+    @Builder
+    Address(String roadName, String addressNickname, String detailedAddress, String postalCode, Boolean isPrimary, User user){
+        this.roadName = roadName;
+        this.addressNickname = addressNickname;
+        this.detailedAddress = detailedAddress;
+        this.postalCode = postalCode;
+        this.isPrimary = isPrimary;
+        this.user = user;
     }
 }
