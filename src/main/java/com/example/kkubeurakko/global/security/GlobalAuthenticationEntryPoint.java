408e6db933f1f1e3aacac8ec7aab7accfe475159
조아빈 <whdkqls122@naver.com>
2024-12-19 16:11:03 +0900

    feat : filter에서 발생하는 예외 처리헨들러 추가 - 필터에서 발생하는 예외는 컨트롤러 이전에 발생하는 예외라 ControllerAdvice로 잡을 수 없어 따로 구현 - SpringSecurity에서 제공하는 ExceptionTranslationHandler를 활용하여 구현

--- src/main/java/com/example/kkubeurakko/global/security/GlobalAuthenticationEntryPoint.java
+++ src/main/java/com/example/kkubeurakko/global/security/GlobalAuthenticationEntryPoint.java
@@ -1,2 +1,28 @@
-package com.example.kkubeurakko.global.security;public class GlobalAuthenticationEntryPoint {
+package com.example.kkubeurakko.global.security;
+
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.example.kkubeurakko.global.common.CommonResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@Component
+public class GlobalAuthenticationEntryPoint implements AuthenticationEntryPoint {
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException authException) throws IOException, ServletException {
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		response.setContentType("application/json");
+		response.getWriter().write(
+			new ObjectMapper().writeValueAsString(
+				new CommonResponse(401, "토큰인증실패", null)
+			)
+		);
+	}
 }
