68f434d6a4714c4a3de222489e7650084414716b
조아빈 <whdkqls122@naver.com>
2024-12-19 18:14:28 +0900

    feat : validation 예외 발생시 처리 헨들러 추가 - GlobalExceptionHandler도 status를 명시하도록 수정

--- src/main/java/com/example/kkubeurakko/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/kkubeurakko/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.kkubeurakko.global.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,10 +16,23 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler({GlobalException.class})
 	protected ResponseEntity<CommonResponse> globalExceptionHandler(GlobalException globalException){
 		log.error(globalException.getGlobalExceptionCode().getResponseMsg());
-		return ResponseEntity.internalServerError().body(
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
 			new CommonResponse(
 				globalException.getGlobalExceptionCode().getCode(),
 				globalException.getGlobalExceptionCode().getResponseMsg(),
+				null
+			)
+		);
+	}
+	@ExceptionHandler({MethodArgumentNotValidException.class})
+	protected ResponseEntity<CommonResponse> validationExceptionHandler(
+		MethodArgumentNotValidException methodArgumentNotValidException
+	){
+		log.error(methodArgumentNotValidException.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+			new CommonResponse(
+				400,
+				methodArgumentNotValidException.getMessage(),
 				null
 			)
 		);
