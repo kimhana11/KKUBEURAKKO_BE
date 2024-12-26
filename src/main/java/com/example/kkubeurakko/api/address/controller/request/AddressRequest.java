e219abba318b41b7283a8fd539756f7e597dbd66
조아빈 <whdkqls122@naver.com>
2024-12-26 18:30:30 +0900

    refactor : 에러 메시지 명확하게 하도록 수정

--- src/main/java/com/example/kkubeurakko/api/address/controller/request/AddressRequest.java
+++ src/main/java/com/example/kkubeurakko/api/address/controller/request/AddressRequest.java
@@ -7,8 +7,8 @@ public record AddressRequest (
 
 	@NotNull(message = "서버 에러") Boolean isPrimary,
 	@NotBlank(message = "배송지명은 공백일 수 없습니다.") String addressNickname,
-	@NotBlank(message = "주소는 공백없이 작성해야 합니다.") String postalCode,
-	@NotBlank(message = "주소는 공백없이 작성해야 합니다.") String roadName,
-	@NotBlank(message = "주소는 공백없이 작성해야 합니다.") String detailedAddress
+	@NotBlank(message = "우편번호는 공백없이 작성해야 합니다.") String postalCode,
+	@NotBlank(message = "도로명주소는 공백없이 작성해야 합니다.") String roadName,
+	@NotBlank(message = "상세주소는 공백없이 작성해야 합니다.") String detailedAddress
 ){
 }
