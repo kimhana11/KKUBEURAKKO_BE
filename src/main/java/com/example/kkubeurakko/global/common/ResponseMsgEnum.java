3fba468ae9fa0c10349f3e831578206bc546dc05
조아빈 <whdkqls122@naver.com>
2025-01-04 23:06:50 +0900

    feat : 인증번호 발급 정상반환 메시지 추가

--- src/main/java/com/example/kkubeurakko/global/common/ResponseMsgEnum.java
+++ src/main/java/com/example/kkubeurakko/global/common/ResponseMsgEnum.java
@@ -8,8 +8,9 @@ import lombok.Getter;
 public enum ResponseMsgEnum {
 	SAVE_ADDRESS_SUCCESS(200, "추가되었습니다."),
 	UPDATE_ADDRESS_SUCCESS(200, "수정되었습니다."),
-	DELETE_ADDRESS_SUCCESS(200, "삭제되었습니다.");
-
+	DELETE_ADDRESS_SUCCESS(200, "삭제되었습니다."),
+	SEND_CERTIFICATION_CODE_SUCCESS(200, "인증번호가 발송되었습니다.");
+	
 	private int code;
 	private String responseMsg;
 }
