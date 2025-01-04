12ee4603787cdb6551ccd0e359f6b8f2d47d6830
조아빈 <whdkqls122@naver.com>
2025-01-04 23:05:29 +0900

    feat : sms 인증번호 요청 api 추가 - 휴대폰 번호를 받는 SmsRequest 추가 - 인증번호 요청을 받는 SmsController 추가 - 인증번호 발급하는 SmsService 추가

--- src/main/java/com/example/kkubeurakko/global/api/service/sms/SmsService.java
+++ src/main/java/com/example/kkubeurakko/global/api/service/sms/SmsService.java
@@ -1,2 +1,20 @@
-package com.example.kkubeurakko.global.api.service.sms;public class SmsService {
+package com.example.kkubeurakko.global.api.service.sms;
+
+import org.springframework.stereotype.Service;
+
+import com.example.kkubeurakko.global.api.controller.sms.request.SmsRequest;
+import com.example.kkubeurakko.global.security.sms.SmsCertificationUtil;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SmsService {
+	private final SmsCertificationUtil smsCertificationUtil;
+
+	public void SendSms(SmsRequest smsRequest){
+		String phoneNum = smsRequest.phoneNum();
+		String certificationCode = Integer.toString((int)(Math.random() * (999_999 - 100_000 + 1))+ 100_000);
+		smsCertificationUtil.sendSMS(phoneNum, certificationCode);
+	}
 }
