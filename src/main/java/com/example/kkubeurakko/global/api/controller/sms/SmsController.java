afb0a77998090947b186e29a0c7287507e80160b
조아빈 <whdkqls122@naver.com>
2025-01-04 23:05:02 +0900

    feat : sms 인증번호 요청 api 추가 - 휴대폰 번호를 받는 SmsRequest 추가 - 인증번호 요청을 받는 SmsController 추가 - 인증번호 발급하는 SmsService 추가

--- src/main/java/com/example/kkubeurakko/global/api/controller/sms/SmsController.java
+++ src/main/java/com/example/kkubeurakko/global/api/controller/sms/SmsController.java
@@ -1,2 +1,35 @@
-package com.example.kkubeurakko.global.api.controller.sms;public class SmsController {
+package com.example.kkubeurakko.global.api.controller.sms;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.kkubeurakko.global.api.controller.sms.request.SmsRequest;
+import com.example.kkubeurakko.global.api.service.sms.SmsService;
+import com.example.kkubeurakko.global.common.CommonResponse;
+import com.example.kkubeurakko.global.common.ResponseMsgEnum;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class SmsController {
+	private final SmsService smsService;
+
+	@PostMapping("/api/sms/send")
+	public ResponseEntity<CommonResponse> SendSms(
+		@RequestBody @Valid SmsRequest smsRequest
+	){
+		smsService.SendSms(smsRequest);
+		return ResponseEntity.status(HttpStatus.OK).body(
+			new CommonResponse(
+				ResponseMsgEnum.SEND_CERTIFICATION_CODE_SUCCESS.getCode(),
+				ResponseMsgEnum.SEND_CERTIFICATION_CODE_SUCCESS.getResponseMsg(),
+				null
+			)
+		);
+	}
 }
--- src/main/java/com/example/kkubeurakko/global/api/controller/sms/request/SmsRequest.java
+++ src/main/java/com/example/kkubeurakko/global/api/controller/sms/request/SmsRequest.java
@@ -1,2 +1,9 @@
-package com.example.kkubeurakko.global.api.controller.sms.request;public record SmsRequest() {
+package com.example.kkubeurakko.global.api.controller.sms.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record SmsRequest(
+	@NotBlank(message = "휴대폰 번호를 입력해주세요")
+	String phoneNum
+) {
 }
