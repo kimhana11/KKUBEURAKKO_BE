47e67e705ea1df10a3176f95f7f8f1e1266f7b17
조아빈 <whdkqls122@naver.com>
2025-01-06 23:54:37 +0900

    feat : sms 인증번호 검사 api 추가 - 인증번호 6자리를 받아서 redis에 저장된 인정번호와 비교하여 검증

--- src/main/java/com/example/kkubeurakko/global/api/controller/sms/SmsController.java
+++ src/main/java/com/example/kkubeurakko/global/api/controller/sms/SmsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.kkubeurakko.global.api.controller.sms.request.SmsRequest;
+import com.example.kkubeurakko.global.api.controller.sms.request.SmsVerify;
 import com.example.kkubeurakko.global.api.service.sms.SmsService;
 import com.example.kkubeurakko.global.common.CommonResponse;
 import com.example.kkubeurakko.global.common.ResponseMsgEnum;
@@ -19,11 +20,27 @@ import lombok.RequiredArgsConstructor;
 public class SmsController {
 	private final SmsService smsService;
 
+	//인증번호 발송 api
 	@PostMapping("/api/sms/send")
 	public ResponseEntity<CommonResponse> SendSms(
 		@RequestBody @Valid SmsRequest smsRequest
 	){
 		smsService.SendSms(smsRequest);
+		return ResponseEntity.status(HttpStatus.OK).body(
+			new CommonResponse(
+				ResponseMsgEnum.SEND_CERTIFICATION_CODE_SUCCESS.getCode(),
+				ResponseMsgEnum.SEND_CERTIFICATION_CODE_SUCCESS.getResponseMsg(),
+				null
+			)
+		);
+	}
+
+	//sms 인증번호 검사 api
+	@PostMapping("/api/sms/verify")
+	public ResponseEntity<CommonResponse> verifyCode(
+		@RequestBody @Valid SmsVerify smsVerify
+	){
+		smsService.verifyCode(smsVerify);
 		return ResponseEntity.status(HttpStatus.OK).body(
 			new CommonResponse(
 				ResponseMsgEnum.SEND_CERTIFICATION_CODE_SUCCESS.getCode(),
