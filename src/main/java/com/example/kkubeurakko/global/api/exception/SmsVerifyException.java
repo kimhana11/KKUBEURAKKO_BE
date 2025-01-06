2d67955a6955a3f202ba5c523cd798d67f0e1e02
조아빈 <whdkqls122@naver.com>
2025-01-06 23:53:24 +0900

    feat : 인증번호 예외 클래스 추가

--- src/main/java/com/example/kkubeurakko/global/api/exception/SmsVerifyException.java
+++ src/main/java/com/example/kkubeurakko/global/api/exception/SmsVerifyException.java
@@ -1,2 +1,10 @@
-package com.example.kkubeurakko.global.api.exception;public class SmsVerifyException {
+package com.example.kkubeurakko.global.api.exception;
+
+import com.example.kkubeurakko.global.common.BadResponseMsgEnum;
+import com.example.kkubeurakko.global.exception.GlobalException;
+
+public class SmsVerifyException extends GlobalException {
+	public SmsVerifyException(){
+		super(BadResponseMsgEnum.USER_NOT_FOUND);
+	}
 }
