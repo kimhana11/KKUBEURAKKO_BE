5e2491abc73f8b85cdb87cd91db38466bcc800cb
조아빈 <whdkqls122@naver.com>
2025-01-09 22:26:34 +0900

    feat: Redis 설정을 위한 RedisConfig 추가
    - LettuceConnectionFactory를 이용한 RedisConnectionFactory 설정
    - StringRedisTemplate Bean 등록
    - RedisProperties를 사용하여 호스트 및 포트 설정

--- src/main/java/com/example/kkubeurakko/global/config/RedisConfig.java
+++ src/main/java/com/example/kkubeurakko/global/config/RedisConfig.java
@@ -3,11 +3,11 @@ package com.example.kkubeurakko.global.config;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,16 +18,13 @@ public class RedisConfig {
 	private final RedisProperties redisProperties;
 
 	@Bean
-	public RedisConnectionFactory redisConnectionFactory(){
+	@Primary
+	public RedisConnectionFactory redisConnectionFactory() {
 		return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
 	}
-	@Bean
-	public RedisTemplate<String, Object> redisTemplate() {
-		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>(); // RedisTemplate 인스턴스 생성
-		redisTemplate.setConnectionFactory(redisConnectionFactory()); // Redis 연결 팩토리 설정
-		redisTemplate.setKeySerializer(new StringRedisSerializer()); // 키를 문자열로 직렬화하도록 설정
-		redisTemplate.setValueSerializer(new StringRedisSerializer()); // 값을 문자열로 직렬화하도록 설정
 
-		return redisTemplate; // 설정이 완료된 RedisTemplate 인스턴스를 반환
+	@Bean
+	public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		return new StringRedisTemplate(redisConnectionFactory);
 	}
 }
