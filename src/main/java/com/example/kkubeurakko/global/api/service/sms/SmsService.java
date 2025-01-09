24723f66ffd04c714360efb544e775a5ff97d28f
조아빈 <whdkqls122@naver.com>
2025-01-09 22:23:57 +0900

    feat : 휴대폰 sms 인증 비즈니스 로직 수행하는 SmsService 추가

--- src/main/java/com/example/kkubeurakko/global/api/service/sms/SmsService.java
+++ src/main/java/com/example/kkubeurakko/global/api/service/sms/SmsService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 
 import com.example.kkubeurakko.global.api.controller.sms.request.SmsRequest;
 import com.example.kkubeurakko.global.api.controller.sms.request.SmsVerify;
+import com.example.kkubeurakko.global.api.exception.SmsCertificationCodeExpiredException;
+import com.example.kkubeurakko.global.api.exception.SmsNotEnoughBalanceException;
 import com.example.kkubeurakko.global.api.exception.SmsVerifyException;
 import com.example.kkubeurakko.global.api.repository.SmsRepository;
 import com.example.kkubeurakko.global.security.sms.SmsCertificationUtil;
@@ -19,10 +21,14 @@ public class SmsService {
 	private final SmsRepository smsRepository;
 
 	public void SendSms(SmsRequest smsRequest){
-		String phoneNum = smsRequest.phoneNum();
-		String certificationCode = Integer.toString((int)(Math.random() * (999_999 - 100_000 + 1))+ 100_000);
-		smsCertificationUtil.sendSMS(phoneNum, certificationCode);
-		smsRepository.createSmsCertification(phoneNum, certificationCode);
+		try{
+			String phoneNum = smsRequest.phoneNum();
+			String certificationCode = Integer.toString((int)(Math.random() * (999_999 - 100_000 + 1))+ 100_000);
+			smsCertificationUtil.sendSMS(phoneNum, certificationCode);
+			smsRepository.createSmsCertification(phoneNum, certificationCode);
+		} catch (Exception e){
+			throw new SmsNotEnoughBalanceException();
+		}
 	}
 
 	public void verifyCode(SmsVerify smsVerify){
@@ -36,7 +42,9 @@ public class SmsService {
 	}
 
 	private boolean isVerify(String phoneNum, String certificationCode){
-		return smsRepository.hasKey(phoneNum) &&
-			smsRepository.getSmsCertification(phoneNum).equals(certificationCode);
+		if(!smsRepository.hasKey(phoneNum)){
+			throw new SmsCertificationCodeExpiredException();
+		}
+		return smsRepository.getSmsCertification(phoneNum).equals(certificationCode);
 	}
 }
