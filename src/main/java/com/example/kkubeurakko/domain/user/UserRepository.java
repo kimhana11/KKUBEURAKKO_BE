6d4781a0d4349404ee74cc0bf2414b8123b9c246
조아빈 <whdkqls122@naver.com>
2025-01-19 12:12:43 +0900

    feat : 저장된 유저의 전화번호가 있는지 판별하기 위한 메서드 추가

--- src/main/java/com/example/kkubeurakko/domain/user/UserRepository.java
+++ src/main/java/com/example/kkubeurakko/domain/user/UserRepository.java
@@ -3,9 +3,14 @@ package com.example.kkubeurakko.domain.user;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.kkubeurakko.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByUserNumber(String userNumber);
+
+	@Query("select case when count(u) > 0 then true else false end from User u where u.userNumber = :userNumber and u.phone is not null")
+	boolean existsUserPhoneByUserNumber(@Param("userNumber") String userNumber);
 }
