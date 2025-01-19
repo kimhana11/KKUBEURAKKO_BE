97efdc2c35d9d1d6dd22d3121efca043ccecec6d
조아빈 <whdkqls122@naver.com>
2025-01-19 12:19:40 +0900

    feat : 유저의 전화번호 저장여부 판별 api 추가 - UserController에 checkExistsUserPhone 추가 - 사용자에게 보여지는 응답이 아니므로 boolean 값만 전달

--- src/main/java/com/example/kkubeurakko/api/controller/user/UserController.java
+++ src/main/java/com/example/kkubeurakko/api/controller/user/UserController.java
@@ -1,2 +1,31 @@
-package com.example.kkubeurakko.api.controller.user;public class UserController {
+package com.example.kkubeurakko.api.controller.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.kkubeurakko.api.service.user.UserService;
+import com.example.kkubeurakko.domain.user.UserRepository;
+import com.example.kkubeurakko.global.common.CommonResponse;
+import com.example.kkubeurakko.global.oauth.dto.CustomOAuth2User;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import retrofit2.http.GET;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class UserController {
+	private final UserService userService;
+	@GetMapping("/api/user/exists-phone")
+	public ResponseEntity<Boolean> checkExistsUserPhone(
+		@AuthenticationPrincipal CustomOAuth2User customOAuth2User
+
+	) {
+		boolean existsUserPhone = userService.checkExistsUserPhone(customOAuth2User);
+		return ResponseEntity.status(HttpStatus.OK).body(existsUserPhone);
+	}
 }
