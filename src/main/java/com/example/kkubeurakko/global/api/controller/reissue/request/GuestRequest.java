9f142ee635f2e6bc704c87db081885f9a8eabb1c
조아빈 <whdkqls122@naver.com>
2025-01-20 18:01:41 +0900

    feat : 비회원 인증 요청시 전달 dto 추가 - 해당 정보들을 어디에 저장하고 사용할지 해결 예정

--- src/main/java/com/example/kkubeurakko/global/api/controller/reissue/request/GuestRequest.java
+++ src/main/java/com/example/kkubeurakko/global/api/controller/reissue/request/GuestRequest.java
@@ -1,2 +1,16 @@
-package com.example.kkubeurakko.global.api.controller.reissue.request;public class GuestRequest {
+package com.example.kkubeurakko.global.api.controller.reissue.request;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+
+public record GuestRequest(
+	@NotBlank(message = "전화번호는 공백일 수 없습니다.")
+	String phoneNum,
+	@NotBlank(message = "우편번호는 공백일 수 없습니다.")
+	String postalCode,
+	@NotBlank(message = "도로명주소는 공백일 수 없습니다.")
+	String roadName,
+	@NotBlank(message = "상세주소는 공백일 수 없습니다.")
+	String detailAddress
+) {
 }
